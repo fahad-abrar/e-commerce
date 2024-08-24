42e1b094315258c64f0a42e0ea7f6fbb50eb8d51
fahad-abrar <fahad.abrarr@gmail.com>
2024-08-24 21:56:49 +0600

    route modifivation

--- router/route.js
+++ router/route.js
@@ -7,105 +7,147 @@ import ReviewController from "../controller/reviewController.js";
 import OrderController from "../controller/orderController.js";
 import FileController from "../controller/fileController.js";
 import upload from "../helper/multer.js";
-import demo from "../controller/demo.js";
 
 const router = express.Router();
 
-router.get("/product", (req, res) => {
-  console.log("its working....");
-  res.status(200).json({
-    msg: "its working",
-  });
-});
+// <<<------------product route -------------- >>>
 
-// product route --- >>>
-router.post("/product/newproduct", catchTry(ProductController.createProduct));
-router.get("/product/get", catchTry(ProductController.getProduct));
-router.get("/product/get/:id", catchTry(ProductController.getProductById));
-router.get("/product/search", catchTry(ProductController.searchProduct));
-router.put("/product/update/:id", catchTry(ProductController.updateProduct));
-router.delete("/product/delete/:id", catchTry(ProductController.deleteProduct));
+router.post(
+  "/product/newproduct",
+  isAuthenticate,
+  catchTry(ProductController.createProduct)
+);
 
-// user route --- >>>
+router.get(
+  "/product/get",
+  isAuthenticate,
+  catchTry(ProductController.getProduct)
+);
+
+router.get(
+  "/product/get/:id",
+  isAuthenticate,
+  catchTry(ProductController.getProductById)
+);
+
+router.get(
+  "/product/search",
+  isAuthenticate,
+  catchTry(ProductController.searchProduct)
+);
+
+router.put(
+  "/product/update/:id",
+  isAuthenticate,
+  catchTry(ProductController.updateProduct)
+);
+
+router.delete(
+  "/product/delete/:id",
+  isAuthenticate,
+  catchTry(ProductController.deleteProduct)
+);
+
+// <<<-----------------user route ----------------------- >>>
+
 router.post("/auth/register", catchTry(UserController.registerUser));
+
 router.put(
   "/auth/update/:id",
   isAuthenticate,
   catchTry(UserController.updateUser)
 );
+
 router.get(
   "/auth/alluser",
   isAuthenticate,
   catchTry(UserController.getAllUser)
 );
+
 router.get(
   "/auth/user/:id",
   isAuthenticate,
   catchTry(UserController.getSingleUser)
 );
+
 router.get("/auth/me", isAuthenticate, catchTry(UserController.getUser));
+
 router.post("/auth/login", catchTry(UserController.logInUser));
+
 router.get("/auth/logout", isAuthenticate, catchTry(UserController.logOutUser));
 
-// password route --- >>>
+//<<<------------------password route --------------------- >>>
+
 router.post("/auth/password/change", catchTry(UserController.changePassword));
+
 router.post("/auth/password/forgot", catchTry(UserController.forgotPassword));
+
 router.post(
   "/auth/password/reset/:token",
   catchTry(UserController.resetPassword)
 );
 
-// review route --- >>>
+// <<<--------------------------review route --------------------- >>>
+
 router.post(
   "/auth/review/:productId",
   isAuthenticate,
   catchTry(ReviewController.createReview)
 );
+
 router.put(
   "/auth/review/update/:id",
   isAuthenticate,
   catchTry(ReviewController.updateReview)
 );
+
 router.delete(
   "/auth/review/delete/:id",
   isAuthenticate,
   catchTry(ReviewController.deleteReview)
 );
+
 router.post(
   "/auth/review/:id",
   isAuthenticate,
   catchTry(ReviewController.updateReview)
 );
 
-// order route --- >>>
+// <<<------------------------order route ------------------------ >>>
+
 router.post(
   "/order/neworder",
   isAuthenticate,
   catchTry(OrderController.newOrders)
 );
+
 router.get(
   "/order/allorder",
   isAuthenticate,
   catchTry(OrderController.getAllOrder)
 );
+
 router.get(
   "/order/singleorder/:id",
   isAuthenticate,
   catchTry(OrderController.getSingleOrder)
 );
+
 router.get("/order/my", isAuthenticate, catchTry(OrderController.getMyOrder));
+
 router.put(
   "/order/update/:id",
   isAuthenticate,
   catchTry(OrderController.updateOrder)
 );
+
 router.delete(
   "/order/delete/:id",
   isAuthenticate,
   catchTry(OrderController.deleteOrder)
 );
 
-// <<< ========  profile route  ======== >>> //
+// <<<-----------------------profile route ------------------------->>> //
 
 // file upload --- >>>
 router.post(
@@ -134,9 +176,13 @@ router.delete(
 );
 
 //get profile avatar
-router.get("/auth/avatar", isAuthenticate, FileController.getprofileFile);
+router.get(
+  "/auth/avatar",
+  isAuthenticate,
+  catchTry(FileController.getprofileFile)
+);
 
-// <<< ========  product route  ======== >>> //
+// <<< -----------------------file route   -------------------->>>
 
 // update file
 router.put(
@@ -162,6 +208,10 @@ router.get(
 );
 
 // get all file
-router.get("/product/image", isAuthenticate, FileController.getallFile);
+router.get(
+  "/product/image",
+  isAuthenticate,
+  catchTry(FileController.getallFile)
+);
 
 export default router;
